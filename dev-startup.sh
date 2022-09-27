89e444abb325bd4e2eac6e6dd1b0dffb2444a7c1
Gabriel Hollies <ghollies@gmail.com>
2022-09-27 11:05:25 -0400

    fix startup script

--- dev-startup.sh
+++ dev-startup.sh
@@ -2,10 +2,10 @@
 set -xe
 if [ ! -e /data/pgdata/ran-migrations ] ; then
     echo "done" > /data/pgdata/ran-migrations
+    mix deps.get
     mix ecto.create
     mix ecto.migrate
     mix run priv/repo/seeds.exs
 fi
 make ni
-mix deps.get
 mix phx.server
--- dev.Dockerfile
+++ dev.Dockerfile
@@ -2,4 +2,4 @@ FROM elixir
 RUN apt-get update -y && apt-get install npm -y
 RUN mix local.hex --force
 WORKDIR /data
-ENTRYPOINT /data/dev-entrypoint.sh
+ENTRYPOINT /data/dev-startup.sh
